780ca5e3275f73476d582e00d1000560c28372a7
Shehryar <shehryarahmed268@gmail.com>
2024-11-26 20:59:35 +0500

    'ui-update'

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,6 +5,10 @@ import { useContext } from 'react'
 import { CartContext } from '../assets/Context/CartContext'
 import { AuthContext } from '../assets/Context/AuthContext'
 import { auth, onAuthStateChanged, signOut } from '../utils/firebase'
+import { FaRegHeart, FaRegUser } from "react-icons/fa";
+import { IoCartOutline } from 'react-icons/io5'
+import { MdOutlineShoppingCart } from 'react-icons/md'
+
 
 const Header = () => {
   const {user, setuser} = useContext(AuthContext)
@@ -52,7 +56,7 @@ const handlelogout = async () => {
     <div className={`sticky top-0 bg-white ${shadow}  z-20`}>
     {/* Main navigation container */}
     <nav className="flex-no-wrap relative flex w-full items-center justify-betwee py-2 shadow-dark-mild lg:flex-wrap lg:justify-start lg:py-4">
-      <div className="flex w-full flex-wrap items-center justify-between px-3">
+      <div className="flex w-full flex-wrap items-center justify-between px-6">
         
           {/* Hamburger icon */}
           <Link to={'/'}><img
@@ -86,6 +90,11 @@ const handlelogout = async () => {
           >
            
           </a>
+          <div className='mx-auto'>
+                        <input onKeyDown={handlesearch} value={search} onChange={(e) => setsearch(e.target.value)} className='border border-gray-500 mr-5 rounded px-2 w-[400px] py-1 placeholder:text-sm placeholder:font-thin focus:border-1 focus:outline-none placeholder:text-gray-700' placeholder='Search Your Item' type="text" name="" id="" />
+                        <button className='bg-blue-500 text-white px-4 py-1 rounded-md'>Search</button>
+
+          </div>
           {/* Left navigation links */}
           
           {/* Left links */}
@@ -93,17 +102,12 @@ const handlelogout = async () => {
         {/* Right elements */}
         <div className="relative flex items-center">
           {/* Icon */}
-            <input onKeyDown={handlesearch} value={search} onChange={(e) => setsearch(e.target.value)} className='border mr-5 rounded px-2 placeholder:text-sm placeholder:font-thin focus:border-1 focus:outline-none' placeholder='Search Your Item' type="text" name="" id="" />
-          <a className=" me-1 text-neutral-600 dark:text-white" href="#">
+            <a className=" me-1 text-neutral-600 dark:text-white" href="#">
           <Link to={'/Cart'}>
-            <span className="[&>svg]:w-5 flex">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill=""
-                >
-                <path d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z" />
-              </svg>
+            <span className="[&>svg]:w-5 flex text-xl mx-2">
+              <MdOutlineShoppingCart />
+
+
               <Badge className='relative -top-3' count={Cartitems.length}/>
             </span>
                 </Link>
@@ -169,9 +173,16 @@ const handlelogout = async () => {
               <Link to={'/signup'}>
             {/* <a href="#"  className='bg-blue-500 rounded px-2 py-1 mx-2 text-white'>Signup</a> */}
             </Link>
-            <Link to={'/login'}>
-            <a href="#"  className='bg-black rounded px-2 py-1 text-white'>Login</a>
+            <div className='flex gap-3'>
+              <Link to={'/login'}>
+            <p className='text-lg'><FaRegHeart /></p>
+
             </Link>
+            <Link to={'/login'}>
+            <p className='text-lg'><FaRegUser /></p>
+
+            </Link>
+            </div>
             </div>
             }
             {/* Second dropdown menu */}
@@ -214,6 +225,11 @@ const handlelogout = async () => {
         {/* Right elements */}
       </div>
     </nav>
+    <div className='flex justify-center px-10 my-4 sm:hidden'>
+                        <input onKeyDown={handlesearch} value={search} onChange={(e) => setsearch(e.target.value)} className='border border-gray-500 mr-5 rounded px-2 w-[400px] py-1 placeholder:text-sm placeholder:font-thin focus:border-1 focus:outline-none placeholder:text-gray-700' placeholder='Search Your Item' type="text" name="" id="" />
+                        <button className='bg-blue-500 text-white px-4 py-1 rounded-md'>Search</button>
+
+          </div>
   </div>
   
 
